e13ee205a0daa9c53948601ef28b51ec633666fc
SariItani <74187768+SariItani@users.noreply.github.com>
2023-05-01 12:31:46 +0300

    get a file from the front end

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
-from flask import Flask, render_template
-
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        # do something with the file
+        return 'File uploaded successfully'
+    else:
+        return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
