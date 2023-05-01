0417ddae44f4ef74161dadc9dab9b30b3711bdbd
SariItani <74187768+SariItani@users.noreply.github.com>
2023-05-01 12:42:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, render_template
+import requests
+import base64
+
+midjourney_api_url = 'https://api.midjourney.com/sketch'
 
 app = Flask(__name__)
 
@@ -7,7 +11,14 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         # do something with the file
-        return 'File uploaded successfully'
+        # Encode the file as Base64
+        encoded_file = base64.b64encode(file.read()).decode('utf-8')
+        # Send the file to the midjourney API
+        response = requests.post(midjourney_api_url, json={'image': encoded_file})
+        # Extract the sketch image from the response
+        sketch_image = response.json()['sketch']
+        # Render the sketch image in the browser
+        return f'<img src="data:image/png;base64,{sketch_image}">'
     else:
         return render_template('index.html')
 
