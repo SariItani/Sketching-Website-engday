d2c756427cc51bdf696fd01ee588f04c6d09c856
SariItani <74187768+SariItani@users.noreply.github.com>
2023-05-02 12:50:36 +0300

    ,

--- positionGetter.py
+++ positionGetter.py
@@ -1,2 +1,10 @@
+from time import sleep
 import pyautogui
+
+screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
+
+
+while(1):
+    currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
+    print(currentMouseX, currentMouseY)
 
