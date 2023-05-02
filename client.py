aa01cabdf0b4ee7221746a71d00e1938285c06ca
SariItani <74187768+SariItani@users.noreply.github.com>
2023-05-02 19:13:13 +0300

    got the image done

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+import base64
+from io import BytesIO
+from flask import Flask, jsonify, render_template, request
 import pyautogui
+from PIL import Image
 
 def getMousePosition():
     return pyautogui.position()
@@ -9,6 +12,41 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload-canvas', methods=['POST'])
+def upload_canvas():
+    dataURL = request.json['dataURL']
+    image_data = base64.b64decode(dataURL.split(',')[1])
+
+    # Load the image
+    with open('image.png', 'wb') as f:
+        f.write(image_data)
+
+    # Get the color of each pixel in the image
+    image = Image.open('image.png')
+    pixels = image.load()
+    colors = []
+    for y in range(image.size[1]):
+        for x in range(image.size[0]):
+            color = pixels[x, y]
+            colors.append(color)
+
+    # colors is now a 1-dimensional array of colors, where each color is a tuple of (R, G, B, A) values
+
+    # Get the size of the image
+    width, height = image.size
+    
+    # Create an image from the colors array
+    new_image = Image.new("RGBA", (width, height))
+    new_image.putdata(colors)
+
+    # Save the image to a PNG file
+    output_buffer = BytesIO()
+    new_image.save(output_buffer, format='PNG')
+    image_data = output_buffer.getvalue()
+
+    return jsonify({'message': 'Image uploaded successfully!'})
+
 
 
 if __name__ == '__main__':
